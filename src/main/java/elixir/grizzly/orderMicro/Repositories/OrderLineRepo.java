034dfb577e7ffbf50b9ff8cab7c8c4ff3253c33c
yangyu823 <yangyu823@gmail.com>
2018-10-08 23:35:10 +1100

    basic API call

--- src/main/java/elixir/grizzly/orderMicro/Repositories/OrderLineRepo.java
+++ src/main/java/elixir/grizzly/orderMicro/Repositories/OrderLineRepo.java
@@ -1,4 +1,14 @@
 package elixir.grizzly.orderMicro.Repositories;
 
-public class OrderLineRepo {
+import elixir.grizzly.orderMicro.Entity.OrderLine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderLineRepo extends JpaRepository<OrderLine, Integer> {
+
+    @Query("SELECT e FROM OrderLine e WHERE e.orderlineId=:id")
+    OrderLine findByOrderlineId(int id);
+
 }
--- src/main/java/elixir/grizzly/orderMicro/Repositories/OrderRepo.java
+++ src/main/java/elixir/grizzly/orderMicro/Repositories/OrderRepo.java
@@ -1,4 +1,15 @@
 package elixir.grizzly.orderMicro.Repositories;
 
-public class OrderRepo {
+import elixir.grizzly.orderMicro.Entity.OrderSum;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepo extends JpaRepository<OrderSum, Integer> {
+
+    @Query("SELECT e FROM OrderSum e WHERE e.orderId=:id")
+    OrderSum findByOrderId(int id);
+
+
 }
